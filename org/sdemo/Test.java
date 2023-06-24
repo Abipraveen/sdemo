ae1d703ec379308605a1d91500e38568b1a489d0
abirami <abiramidayalan18@gmail.com>
2023-06-24 14:27:02 +0530

    sdemo

---
+++ org/sdemo/Test.java
@@ -0,0 +1,12 @@
+package org.sdemo;  
+public class Test{
+public static void main(String[] args)
+{
+	
+		System.out.println("Phone Details of a person");
+		//System.out.println("Phone Id is:" + id+ "\nPerson Name is:"+name + "\nPerson MobileNumber: " +phonenum+ "\nAddress" +address);
+	
+	
+}
+
+}
